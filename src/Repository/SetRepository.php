1334f72111689530694e9d4c343f6c67a3025246
Mael <mael.brevet@gmail.com>
2019-05-03 11:45:10 +0200

    Fix the name 'set' none autorized

--- src/Repository/SetRepository.php
+++ src/Repository/SetRepository.php
@@ -4,12 +4,12 @@
 namespace App\Repository;
 
 
-use App\Entity\Set;
+use App\Entity\SetPoint;
 
 interface SetRepository
 {
     public function getAll(): array;
-    public function getById(int $setId): Set;
-    public function save(Set $set): void;
-    public function remove(Set $set): void;
+    public function getById(int $setId): SetPoint;
+    public function save(SetPoint $set): void;
+    public function remove(SetPoint $set): void;
 }
