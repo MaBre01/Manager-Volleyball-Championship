f5fbc09fd77dd4ceac9dc8ac7143a8b2bbcc6ba5
Mael <mael.brevet@gmail.com>
2019-05-06 14:13:33 +0200

    Add team fixtures with account

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Account;
 use App\Entity\Club;
+use App\Entity\Team;
+use App\Entity\TeamManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\Query\Expr\Math;
 use Faker;
 
 class AppFixtures extends Fixture
@@ -18,8 +22,27 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < $nbClub; $i++){
             $club = new Club(0, $faker->city);
 
+            for( $i = 0; $i < random_int(1,4); $i++){
+
+                $teamManager = new TeamManager($faker->firstName, $faker->lastName, $faker->phoneNumber);
+
+                $email = $teamManager->getFirstName() . "." . $teamManager->getLastName() . "@gmail.com";
+                $account = new Account($email, "123456", ['ROLE_TEAM']);
+
+                $manager->persist($account);
+
+                $teamName = $club->getName() . "--" . $i;
+                $team = new Team(0, $teamName, $club, true, $account , $teamManager);
+
+                $manager->persist( $team );
+            }
+
             $manager->persist( $club );
         }
+
+        /* Add admin account */
+        $admin = new Account("admin@gmail.com", "123456", ['ROLE_ADMIN']);
+        $manager->persist( $admin );
 
         $manager->flush();
     }
